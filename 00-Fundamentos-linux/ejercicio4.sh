cd4f1578d4de750c7db17bc01df274ba30cef3de
josekuma <josekgj@gmail.com>
2023-01-12 20:36:54 +0100

    Cuarto Ejercicio

---
+++ 00-Fundamentos-linux/ejercicio4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [$# -ne 1]
+then
+echo "Error: you need to provide ONE argument at least."
+exit 1
+fi
+
+TEXTO=$(curl -s http://metaphorpsum.com/paragraphs/2/4)
+echo $TEXTO
+echo "La palabra $(echo $1) aparece $(echo $TEXTO | grep -ow $1 | wc -w) veces"
+echo "Aparece por primera vez en la línea $(echo $TEXTO | grep -n $1| head -c 1)"
